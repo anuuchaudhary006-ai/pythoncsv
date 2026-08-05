d40a1bb4223fc10d25f41b4c9484b4152f6f16b5
Anisha Chaudhary <anuuchaudhary006@gmail.com>
2026-08-05 08:46:52 +0530

    csv write file

--- abcd.py
+++ abcd.py
@@ -1,6 +1,16 @@
 # CSV -> comma separated Value
 import csv
-f = open("j.csv","w",newline='')
-writer = csv.writer(f)
-writer.writerow(["SNO","NAME","COURSE"])
-writer.writerow([1,"Anisha","MERN"])
+def WriteCSV():
+    f = open("j.csv","w",newline='')
+    writer = csv.writer(f)
+    writer.writerow(["SNO","NAME","COURSE"])
+    writer.writerow([1,"Anisha","MERN"])
+    writer.writerow([2,"Anshul","PERN"])
+    writer.writerow([3,"Akshay","MEAN"])
+    writer.writerow([4,"Aman","Python"])
+
+
+f = open("j.csv",'r')
+r = csv.reader(f)
+for i in r:
+    print(i)
