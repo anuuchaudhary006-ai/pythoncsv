7540b8909d6a6b5d5304973e661382bc1bea427f
Anisha Chaudhary <anuuchaudhary006@gmail.com>
2026-08-05 09:17:12 +0530

    csv write file

--- abcd.py
+++ abcd.py
@@ -10,7 +10,37 @@ def WriteCSV():
     writer.writerow([4,"Aman","Python"])
 
 
-f = open("j.csv",'r')
-r = csv.reader(f)
-for i in r:
-    print(i)
+def ReadCSV():
+    f = open("j.csv",'r')
+    r = csv.reader(f)
+    for i in r:
+        print(i)
+
+
+def AppendCSV():
+    f = open("j.csv","a",newline='')
+    d = csv.writer(f)
+    d.writerow([5,"Anurag","Java"])
+
+choice = True
+while choice:
+    print("1. Write CSV")
+    print("2. Read CSV")
+    print("3. Append CSV")
+    print("4. EXIT")
+
+    choice1= int(input("enter your choice:"))
+    
+    if choice1 ==1:
+        WriteCSV()
+    elif choice1 ==2:
+        ReadCSV()
+    elif choice1 ==3:
+        AppendCSV()
+    elif choice1 ==4:
+        choice = False
+    else:
+
+        print("Invalid Choice, please enter a number between 1 and 3.")
+
+    
